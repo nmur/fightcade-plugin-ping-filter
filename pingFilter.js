3c552dcd494604ac502a160c2a2dbc1c98a0a186
Nicholas Murray <nickmurray91@gmail.com>
2025-03-25 11:55:08 +1100

    Update pingFilter.js

--- pingFilter.js
+++ pingFilter.js
@@ -2,6 +2,11 @@ const config = require('./config.json');
 
 module.exports = (FCADE) => { runPlugin(FCADE) };
 
+const defaultConfig = {
+    users: 150,
+    matches: 150
+};
+
 const runPlugin = (FCADE) => {
     // Plugin code goes here
     setInterval(() => {
@@ -9,19 +14,23 @@ const runPlugin = (FCADE) => {
     }, 500);
 }
 
-const filterUsers = (FCADE, pingLimit) => {
-    if (pingLimit.users > 0) {
+const filterUsers = (FCADE, pingFilterConfig) => {
+    if (!pingFilterConfig) {
+        pingFilterConfig = defaultConfig
+    }
+
+    if (pingFilterConfig.users > 0) {
         FCADE.$refs[FCADE.activeChannelId][0]?.$refs?.usersList.users.forEach(user => {
-            if (user.ping > pingLimit.users) { 
+            if (user.ping > pingFilterConfig.users) { 
                 FCADE.$refs[FCADE.activeChannelId][0]?.$refs?.usersList.removeUser(user);
             }
         });
     }
 
-    if (pingLimit.matches > 0) {
+    if (pingFilterConfig.matches > 0) {
         FCADE.$refs[FCADE.activeChannelId][0]?.$refs?.usersList.matches.forEach(match => {
-            if ((match.player1.name == "<offline>" || match.player1.ping > pingLimit.matches) && 
-                (match.player2.name == "<offline>" || match.player2.ping > pingLimit.matches)) { 
+            if ((match.player1.name == "<offline>" || match.player1.ping > pingFilterConfig.matches) && 
+                (match.player2.name == "<offline>" || match.player2.ping > pingFilterConfig.matches)) { 
                 FCADE.$refs[FCADE.activeChannelId][0]?.$refs?.usersList.removeMatch(match);
             }
         });
